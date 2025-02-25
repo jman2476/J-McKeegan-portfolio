8849518e23b532a6ff7dd6431fb271baf538019b
Squirblechem <152367545+jman2476@users.noreply.github.com>
2025-02-25 17:04:22 -0500

    added contact info to footer, and fixed contact me link error

--- JM_portfolio/src/components/Header.jsx
+++ JM_portfolio/src/components/Header.jsx
@@ -5,13 +5,15 @@ function Header() {
     return (
       <>
         <header className="">
-          <h1>Jeremy McKeegan</h1>
+          <a href="/">
+            <h1>Jeremy McKeegan</h1>
+          </a>
 
           <nav>
               <a href="/aboutMe">About Me</a>
               <a href="/portfolio">Portfolio</a>
-              <a href="/ contactMe">Contact Me</a>
-              <a href="">Resume </a>
+              <a href="/contactMe">Contact Me</a>
+              <a href="">Resume</a>
           </nav>
         </header>
       </>
